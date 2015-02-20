6b2c96f87538bd07d886e6c4d07c26166507008f
damoeb <maggo.ruepp@gmail.com>
2015-02-20 14:14:34 +0100

    fix

--- dump.js
+++ dump.js
@@ -11,18 +11,6 @@ address = system.args[1];
 output = system.args[2];
 page.viewportSize = { width: 1024, height: 768 };
 
-/*
-if (system.args.length > 3 && system.args[2].substr(-4) === ".pdf") {
-    size = system.args[3].split('*');
-    page.paperSize = size.length === 2 ? { width: size[0], height: size[1], margin: '0px' }
-        : { format: system.args[3], orientation: 'portrait', margin: '1cm' };
-}
-
-if (system.args.length > 4) {
-    page.zoomFactor = system.args[4];
-}
-*/
-
 page.open(address, function (status) {
     if (status !== 'success') {
         console.log('Unable to load the address!');
@@ -30,7 +18,7 @@ page.open(address, function (status) {
     } else {
         window.setTimeout(function () {
             var content = page.content;
-            console.log('Content: ' + content);
+            console.log(content);
             phantom.exit();
         }, 2000);
     }
